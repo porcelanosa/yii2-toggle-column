f32d4cc8d4584b74ac84a8997f91a1a9829c2828
porcelanosa <public_mce@rambler.ru>
2018-07-18 12:23:27 +0300

    Before resuming work

--- ToggleAction.php
+++ ToggleAction.php
@@ -10,6 +10,7 @@ namespace porcelanosa\yii2togglecolumn;
 use Yii;
 use yii\base\Action;
 use yii\base\InvalidConfigException;
+use yii\db\Expression;
 use yii\web\MethodNotAllowedHttpException;
 
 class ToggleAction extends Action
@@ -18,57 +19,57 @@ class ToggleAction extends Action
 	 * @var string name of the model
 	 */
 	public $modelClass;
-
+	
 	/**
 	 * @var string model attribute
 	 */
 	public $attribute = 'active';
-
+	
 	/**
 	 * @var string scenario model
 	 */
 	public $scenario = null;
-
+	
 	/**
 	 * @var string|array additional condition for loading the model
 	 */
 	public $andWhere;
-
+	
 	/**
-	 * @var string|int|boolean what to set active models to
+	 * @var string|int|boolean|Expression what to set active models to
 	 */
 	public $onValue = 1;
-
+	
 	/**
 	 * @var string|int|boolean what to set inactive models to
 	 */
 	public $offValue = 0;
-
+	
 	/**
 	 * @var bool whether to set flash messages or not
 	 */
 	public $setFlash = false;
-
+	
 	/**
 	 * @var string flash message on success
 	 */
 	public $flashSuccess = "Model saved";
-
+	
 	/**
 	 * @var string flash message on error
 	 */
 	public $flashError = "Error saving Model";
-
+	
 	/**
 	 * @var string|array URL to redirect to
 	 */
 	public $redirect;
-
+	
 	/**
 	 * @var string pk field name
 	 */
 	public $primaryKey = 'id';
-
+	
 	/**
 	 * Run the action
 	 * @param $id integer id of model to be loaded
@@ -84,43 +85,40 @@ class ToggleAction extends Action
 		}
 		$id = (int)$id;
 		$result = null;
-
+		
 		if (empty($this->modelClass) || !class_exists($this->modelClass)) {
 			throw new InvalidConfigException("Model class doesn't exist");
 		}
 		/* @var $modelClass \yii\db\ActiveRecord */
-		$modelClass = $this->modelClass; //  текущая модель
-
-
+		$modelClass = $this->modelClass;
+		
+		
 		$attribute = $this->attribute;
 		$model = $modelClass::find()->where([$this->primaryKey => $id]);
-
-		/* если есть дополнительное условие */
+		
 		if (!empty($this->andWhere)) {
 			$model->andWhere($this->andWhere);
 		}
-
-		/*получение записи*/
+		
 		$model = $model->one();
-
-		// если есть сценарий - добавляем к модели сценарий
+		
 		if (!is_null($this->scenario)) {
 			$model->scenario = $this->scenario;
 		}
-
-		if (!$model->hasAttribute($this->attribute)) { //  ошибка в конфигурации - не указан (или неправильно указан) атрибут (имя поля)
-			throw new InvalidConfigException("Attribute ".$this->attribute." doesn't exist");
+		
+		if (!$model->hasAttribute($this->attribute)) {
+			throw new InvalidConfigException("Attribute doesn't exist");
 		}
-
-		/* переключаем значение */
+		
 		if ($model->$attribute == $this->onValue) {
+			$model->$attribute = $this->offValue;
+		} elseif ($this->onValue instanceof Expression && $model->$attribute != $this->offValue) {
 			$model->$attribute = $this->offValue;
 		} else {
 			$model->$attribute = $this->onValue;
 		}
-
-		/*  сохраняем модель */
-		if ($model->save(false , [$this->attribute])) {
+		
+		if ($model->save()) {
 			if ($this->setFlash) {
 				Yii::$app->session->setFlash('success', $this->flashSuccess);
 			}
--- ToggleColumn.php
+++ ToggleColumn.php
@@ -1,92 +1,148 @@
 <?php
-/**
- * @link https://github.com/porcelanosa/yii2-toggle-column
- * @copyright Copyright (c) 2016 Porcelanosa
- * @license MIT http://opensource.org/licenses/MIT
- *
- * USAGE: 
- */
-
-namespace porcelanosa\yii2togglecolumn;
-
-use yii\grid\DataColumn;
-use yii\helpers\Html;
-use yii\web\View;
-use Yii;
-
-class ToggleColumn extends DataColumn
-{
 	/**
-	 * Toggle action that will be used as the toggle action in your controller
-	 * @var string
+	 * @link      https://github.com/porcelanosa/yii2-toggle-column
+	 * @copyright Copyright (c) 2016 Porcelanosa
+	 * @license   MIT http://opensource.org/licenses/MIT
+	 *
+	 * USAGE:
 	 */
-	public $action = 'toggle';
-
-
+	
+	namespace porcelanosa\yii2togglecolumn;
+	
+	
+	use yii\grid\DataColumn;
+	use yii\helpers\Html;
+	use yii\web\View;
+	use Yii;
+	
 	/**
-	 * @var string pk field name
+	 * @author Aris Karageorgos <aris@phe.me>
 	 */
-	public $primaryKey = 'primaryKey';
-
-	/**
-	 * Whether to use ajax or not
-	 * @var bool
-	 */
-	public $enableAjax = true;
-
-	public function init()
-	{
-		if ($this->enableAjax) {
-			$this->registerJs();
+	class ToggleColumn extends DataColumn {
+		/**
+		 * Toggle action that will be used as the toggle action in your controller
+		 *
+		 * @var string
+		 */
+		public $action = 'toggle';
+		
+		
+		/**
+		 * @var string pk field name
+		 */
+		public $primaryKey = 'primaryKey';
+		
+		/**
+		 * Whether to use ajax or not
+		 *
+		 * @var bool
+		 */
+		public $enableAjax = true;
+		
+		/**
+		 * @var string glyphicon for 'on' value
+		 */
+		public $iconOn = 'ok';
+		
+		/**
+		 * @var string glyphicon for 'off' value
+		 */
+		public $iconOff = 'remove';
+		
+		/**
+		 * @var string text to display on the 'on' link
+		 */
+		public $onText;
+		
+		/**
+		 * @var string text to display on the 'off' link
+		 */
+		public $offText;
+		
+		/**
+		 * @var string text to display next to the 'on' link
+		 */
+		public $displayValueText = false;
+		
+		/**
+		 * @var string text to display next to the 'on' link
+		 */
+		public $onValueText;
+		
+		/**
+		 * @var string text to display next to the 'off' link
+		 */
+		public $offValueText;
+		
+		
+		public function init() {
+			if ( $this->onText === null ) {
+				$this->onText = 'On';
+			}
+			if ( $this->offText === null ) {
+				$this->offText = 'Off';
+			}
+			if ( $this->onValueText === null ) {
+				$this->onValueText = 'Active';
+			}
+			if ( $this->offValueText === null ) {
+				$this->offValueText = 'Inactive';
+			}
+			if ( $this->enableAjax ) {
+				$this->registerJs();
+			}
 		}
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	protected function renderDataCellContent($model, $key, $index)
-	{
-		$url = [$this->action, 'id' => $model->{$this->primaryKey}];
-
-		$attribute = $this->attribute;
-		$value = $model->$attribute;
-
-		if ($value === null || $value == true) {
-			$icon = 'ok';
-			$colorStyle = ' style="color:green" ';
-			$title = Yii::t('yii', 'Off');
-		} else {
-			$icon = 'remove';
-			$colorStyle = ' style="color:red" ';
-			$title = Yii::t('yii', 'On');
+		
+		/**
+		 * @inheritdoc
+		 */
+		protected function renderDataCellContent( $model, $key, $index ) {
+			$url = [ $this->action, 'id' => $model->{$this->primaryKey} ];
+			
+			$attribute = $this->attribute;
+			$value     = $model->$attribute;
+			
+			if ( $value === null || $value == true ) {
+				$colorStyle = ' style="color:green" ';
+				$icon       = $this->iconOn;
+				$title      = $this->offText;
+				$valueText  = $this->onValueText;
+			} else {
+				$colorStyle = ' style="color:red" ';
+				$icon       = $this->iconOff;
+				$title      = $this->onText;
+				$valueText  = $this->offValueText;
+			}
+			
+			return Html::a(
+					'<span class="glyphicon glyphicon-' . $icon . '" ' . $colorStyle . '></span>',
+					$url,
+					[
+						'title'       => $title,
+						'class'       => 'toggle-column',
+						'data-method' => 'post',
+						'data-pjax'   => '0',
+					]
+				) . ( $this->displayValueText ? " {$valueText}" : "" );
 		}
-		return Html::a(
-			'<span class="glyphicon glyphicon-' . $icon . '" ' . $colorStyle . '></span>',
-			$url,
-			[
-				'title' => $title,
-				'class' => 'toggle-column',
-				'data-method' => 'post',
-				'data-pjax' => '0',
-			]
-		);
-	}
-
-	/**
-	 * Registers the ajax JS
-	 */
-	public function registerJs()
-	{
-		$js = <<<'JS'
-			$("a.toggle-column").on("click", function(e) {
+		
+		/**
+		 * Registers the ajax JS
+		 */
+		public function registerJs() {
+			if ( Yii::$app->request->isAjax ) {
+				return;
+			}
+			$js = <<<'JS'
+			$(document.body).on("click", "a.toggle-column", function(e) {
 			    e.preventDefault();
 			    $.post($(this).attr("href"), function(data) {
-			        var pjaxId = $(e.target).closest(".grid-view").parent().attr("id");
+			        var pjaxId = $(e.target).closest("[data-pjax-container]").attr("id");
 			        $.pjax.reload({container:"#" + pjaxId});
 			    });
-			    return false;
-			});
+    return false;
+});
 JS;
-		$this->grid->view->registerJs($js, View::POS_READY, 'yii-toggle-column');
+			$this->grid->view->registerJs( $js, View::POS_READY, 'pheme-toggle-column' );
+		}
 	}
-}
